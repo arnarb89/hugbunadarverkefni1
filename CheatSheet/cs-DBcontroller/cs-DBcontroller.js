a99ed065ffe84a887441e27119361342551b5b4d
Arnar Barri Símonarson <arnarb89@gmail.com>
2016-11-05 21:49:31 +0000

    deleted comments from cs-DBcontroller.js

--- CheatSheet/cs-DBcontroller/cs-DBcontroller.js
+++ CheatSheet/cs-DBcontroller/cs-DBcontroller.js
@@ -1,7 +1,5 @@
 'use strict';
 var pg = require('pg');
-//var conString = 'postgres://krkexcjmicdyea:ctHjYGsxEs581nw_TFG-Wc-e8X@ec2-46-137-72-123.eu-west-1.compute.amazonaws.com:5432/d7notqdphhfuja';
-//var conString = 'postgres://postgres:swag@localhost:5432/v6';
 var conString = process.env.DB_HOST;
 
 exports.queryDb = function(queryStr, parameters, then) {
