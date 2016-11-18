35ff2ad6164d168b0fc072c1f661a2c3d16ace2f
Arnar Barri Símonarson <arnarb89@gmail.com>
2016-11-18 22:25:02 +0000

    stylized the banner and worked slightly on the account management things

--- CheatSheet/routes/account-router.js
+++ CheatSheet/routes/account-router.js
@@ -1,10 +1,43 @@
 'use strict';
 
-var accountManager = require('../lib/managers/comment-manager');
+var accountManager = require('../lib/managers/account-manager');
 var router = require('express').Router();
-var isAuthenticated = require('../lib/isAuthenticated');
+var userManager = require('../cs-managers/userManager.js');
+var bCrypt = require('bcrypt-nodejs');
 
-router.get('/', isAuthenticated, function (req, res) {
-	// Render account settings page
-	// accountManager
-});
+
+var isNotAuthenticated = function (req, res, next) {
+	'use strict';
+	if (req.isAuthenticated()){
+		res.redirect('/');
+	}
+	return next();
+};
+
+
+module.exports = function(passport){
+	'use strict';
+
+	/* GET login page. */
+	router.get('/', function(req, res) {
+		if(req.isAuthenticated()){
+			res.redirect('/');
+		}
+		else{
+			var renderData = {};
+			res.render('placeholder-account', { renderData, message: req.flash('message') });
+		}
+    	
+	});
+
+	/* Handle Login POST */
+	router.post('/', isNotAuthenticated, function(req,res){
+
+
+
+	}
+	);
+
+
+	return router;
+};
